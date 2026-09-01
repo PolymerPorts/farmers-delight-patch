94b6aaa376f88f9024c7042497dc5785ac30edc3
Patbox <39821509+Patbox@users.noreply.github.com>
2026-09-01 19:56:27 +0200

    Fix crash with latest Farmer's Delight

--- src/main/java/eu/pb4/farmersdelightpatch/mixin/mod/CanvasSignBlockMixin.java
+++ src/main/java/eu/pb4/farmersdelightpatch/mixin/mod/CanvasSignBlockMixin.java
@@ -5,8 +5,11 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,10 +21,16 @@ import vectorwing.farmersdelight.common.block.WallHangingCanvasSignBlock;
 import vectorwing.farmersdelight.common.block.state.CanvasSign;
 
 @Mixin({StandingCanvasSignBlock.class, CeilingHangingCanvasSignBlock.class, WallCanvasSignBlock.class, WallHangingCanvasSignBlock.class})
-public class CanvasSignBlockMixin {
-    @Inject(method = "setPlacedBy", at = @At("TAIL"))
-    private void resyncBlockEntity(Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack, CallbackInfo ci) {
-        if (placer instanceof ServerPlayer player && level.getBlockEntity(pos) instanceof BlockEntity be && be instanceof CanvasSign canvasSign && canvasSign.isDarkBackground()) {
+public abstract class CanvasSignBlockMixin extends Block {
+    public CanvasSignBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity by, ItemStack itemStack) {
+        super.setPlacedBy(level, pos, state, by, itemStack);
+
+        if (by instanceof ServerPlayer player && level.getBlockEntity(pos) instanceof BlockEntity be && be instanceof CanvasSign canvasSign && canvasSign.isDarkBackground()) {
             player.connection.send(be.getUpdatePacket());
         }
     }
